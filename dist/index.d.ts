d0da5b50198c345210549c1bdd9a5b81111e33c5
Munawwar <munawwarfiroz@hotmail.com>
2025-11-22 11:57:46 +0400

    v2 - updated to zustand v5, react 18+, typescript 5+

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,13 +1,14 @@
-import type { SetState, State, StateCreator, StoreApi, Subscribe, UseStore } from "zustand";
-interface StoreMethods<TState extends State> {
-    useGlobalState<Key extends keyof TState>(selector: Key, comparator?: (a: unknown, b: unknown) => boolean): TState[Key];
-    useStore: UseStore<TState>;
+import type { StateCreator, StoreApi } from "zustand";
+import type { UseBoundStore } from "zustand/react";
+interface StoreMethods<TState extends object> {
+    useGlobalState<Key extends keyof TState>(selector: Key, comparator?: (a: TState[Key], b: TState[Key]) => boolean): TState[Key];
+    useStore: UseBoundStore<StoreApi<TState>>;
     getStates(): TState;
     setStates(newStore: TState): void;
     updateStates(partial: Partial<TState>, updater?: (partial: Partial<TState>) => void): void;
     createPropUpdater<Prop extends keyof TState>(propName: Prop): (partial: Partial<TState[Prop]>, updater?: (partial: Partial<TState>) => void) => void;
-    subscribe: Subscribe<TState>;
+    subscribe: StoreApi<TState>["subscribe"];
 }
-export declare const createStore: <TState extends Record<string | number | symbol, unknown>>(initState: TState, initialStateCreator?: StateCreator<TState, SetState<TState>> | StoreApi<TState>) => StoreMethods<TState>;
-export declare const useGlobalState: <Key extends never>(selector: Key, comparator?: ((a: unknown, b: unknown) => boolean) | undefined) => {}[Key], getStates: () => {}, setStates: (newStore: {}) => void, updateStates: (partial: Partial<{}>, updater?: ((partial: Partial<{}>) => void) | undefined) => void, createPropUpdater: <Prop extends never>(propName: Prop) => (partial: Partial<{}[Prop]>, updater?: ((partial: Partial<{}>) => void) | undefined) => void, useStore: UseStore<{}>, subscribe: Subscribe<{}>;
+export declare const createStore: <TState extends object>(initState: TState, initialStateCreator?: StateCreator<TState, [], []>) => StoreMethods<TState>;
+export declare const useGlobalState: <Key extends never>(selector: Key, comparator?: ((a: {}[Key], b: {}[Key]) => boolean) | undefined) => {}[Key], getStates: () => {}, setStates: (newStore: {}) => void, updateStates: (partial: Partial<{}>, updater?: ((partial: Partial<{}>) => void) | undefined) => void, createPropUpdater: <Prop extends never>(propName: Prop) => (partial: Partial<{}[Prop]>, updater?: ((partial: Partial<{}>) => void) | undefined) => void, useStore: UseBoundStore<StoreApi<{}>>, subscribe: (listener: (state: {}, prevState: {}) => void) => () => void;
 export {};
